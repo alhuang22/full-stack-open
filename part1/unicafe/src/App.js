d3c86a884d068dc7b92e9448278e2769076d4a8c
Alex Huang <alexxh0098@gmail.com>
2021-05-24 17:09:28 -0700

    exercise 1.8: refactor stats into own component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,27 +4,14 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
-
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = total !== 0 ? (good + bad * -1) / total : 0;
   const positive = total !== 0 ? good / total : 0;
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button text="good" handleClick={handleGood} />
-      <Button text="neutral" handleClick={handleNeutral} />
-      <Button text="bad" handleClick={handleBad} />
-      <h1>statistics</h1>
       <p>
+        <h1>statistics</h1>
         good {good}
         <br />
         neutral {neutral}
@@ -35,8 +22,29 @@ const App = () => {
         <br />
         average {average}
         <br />
-        positive {positive}
+        positive {positive} %
       </p>
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
