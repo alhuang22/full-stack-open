21c2a8db2c31ff70d605e680b380bd98e9651227
Alex Huang <alexxh0098@gmail.com>
2021-05-25 16:05:39 -0700

    finish 2.3, add explanation for reduce

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -15,6 +15,10 @@ const Header = ({ course }) => {
 };
 
 const Total = ({ course }) => {
+  /*
+    (a, b) where a is the accumulator and b is the current value in the array
+    the 0 passed in at the end indicates the initial value of the accumulator
+  */
   let sum = course.parts.reduce((a, b) => a + b.exercises, 0);
   return <p>total of {sum} exercises </p>;
 };
